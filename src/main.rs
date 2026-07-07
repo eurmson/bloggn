5964a4924fd337471176ee9c8d9c913dc78e495d
Ethan Urmson <eurmson1@gmail.com>
2026-07-06 23:21:52 -0700

    fixed favicon path

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket_dyn_templates::{Template, context};
 use rocket::fs::FileServer;
+use rocket_dyn_templates::{Template, context};
 mod actions;
+mod auth;
 mod db;
 mod models;
 mod schema; // This will be generated later
-mod auth;
 
 use db::DbConn;
 
@@ -71,19 +71,27 @@ fn blog_post(mut conn: DbConn, user: Option<auth::AdminUser>, id: i32) -> Option
 
 #[get("/static/output.css")]
 fn stylesheet() -> (rocket::http::ContentType, &'static str) {
-    (rocket::http::ContentType::CSS, include_str!(concat!(env!("OUT_DIR"), "/output.css")))
+    (
+        rocket::http::ContentType::CSS,
+        include_str!(concat!(env!("OUT_DIR"), "/output.css")),
+    )
 }
 
 #[get("/favicon.svg")]
 fn favicon_svg() -> (rocket::http::ContentType, &'static str) {
-    (rocket::http::ContentType::SVG, include_str!("../static/favicon.svg"))
+    (
+        rocket::http::ContentType::SVG,
+        include_str!("../favicon.svg"),
+    )
 }
 
 #[get("/favicon.ico")]
 fn favicon_ico() -> (rocket::http::ContentType, &'static str) {
-    (rocket::http::ContentType::SVG, include_str!("../static/favicon.svg"))
+    (
+        rocket::http::ContentType::SVG,
+        include_str!("../favicon.svg"),
+    )
 }
-
 
 #[launch]
 fn rocket() -> _ {
@@ -95,17 +103,18 @@ fn rocket() -> _ {
         if args[1] == "--list-passkeys" {
             let pool = db::init_pool();
             let mut conn = pool.get().expect("Failed to get DB connection");
-            
-            use diesel::prelude::*;
+
             use crate::schema::passkeys::dsl::*;
-            let list = passkeys.load::<crate::models::PasskeyModel>(&mut conn)
+            use diesel::prelude::*;
+            let list = passkeys
+                .load::<crate::models::PasskeyModel>(&mut conn)
                 .expect("Failed to load passkeys");
-            
+
             println!("Registered Passkeys:");
             println!("------------------------------------------------------------");
             for pk in list {
                 println!(
-                    "ID: {} | User: {} | Authorized: {}", 
+                    "ID: {} | User: {} | Authorized: {}",
                     pk.id, pk.username, pk.authorized
                 );
             }
@@ -115,16 +124,19 @@ fn rocket() -> _ {
             let cred_id = &args[2];
             let pool = db::init_pool();
             let mut conn = pool.get().expect("Failed to get DB connection");
-            
-            use diesel::prelude::*;
+
             use crate::schema::passkeys::dsl::*;
-            
+            use diesel::prelude::*;
+
             let count = diesel::update(passkeys.filter(id.eq(cred_id)))
                 .set(authorized.eq(true))
                 .execute(&mut conn)
                 .expect("Failed to update passkeys");
-                
-            println!("Successfully authorized {} passkey(s) with ID '{}'.", count, cred_id);
+
+            println!(
+                "Successfully authorized {} passkey(s) with ID '{}'.",
+                count, cred_id
+            );
             std::process::exit(0);
         } else if args[1] == "--backfill-images" {
             backfill_image_dimensions();
@@ -134,42 +146,48 @@ fn rocket() -> _ {
 
     let pool = db::init_pool();
     {
-        use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+        use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
         pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
-        let mut conn = pool.get().expect("Failed to get DB connection for migrations");
-        conn.run_pending_migrations(MIGRATIONS).expect("Failed to run database migrations");
+        let mut conn = pool
+            .get()
+            .expect("Failed to get DB connection for migrations");
+        conn.run_pending_migrations(MIGRATIONS)
+            .expect("Failed to run database migrations");
     }
 
     let image_dir = std::env::var("IMAGE_DIR").unwrap_or_else(|_| "static".to_string());
     let webauthn = auth::init_webauthn();
 
     rocket::build()
-        .mount("/", routes![
-            index, 
-            blog, 
-            blog_partial, 
-            blog_post,
-            stylesheet,
-            favicon_svg,
-            favicon_ico,
-            auth::admin_login,
-            auth::login_start,
-            auth::login_finish,
-            auth::admin_register,
-            auth::register_start,
-            auth::register_finish,
-            auth::admin_dashboard,
-            auth::admin_logout,
-            auth::new_post,
-            auth::create_post_handler,
-            auth::edit_post,
-            auth::update_post_handler,
-            auth::delete_post_handler,
-            auth::upload_image_handler,
-            auth::delete_image_handler,
-            auth::update_profile_handler,
-            auth::admin_redirect,
-        ])
+        .mount(
+            "/",
+            routes![
+                index,
+                blog,
+                blog_partial,
+                blog_post,
+                stylesheet,
+                favicon_svg,
+                favicon_ico,
+                auth::admin_login,
+                auth::login_start,
+                auth::login_finish,
+                auth::admin_register,
+                auth::register_start,
+                auth::register_finish,
+                auth::admin_dashboard,
+                auth::admin_logout,
+                auth::new_post,
+                auth::create_post_handler,
+                auth::edit_post,
+                auth::update_post_handler,
+                auth::delete_post_handler,
+                auth::upload_image_handler,
+                auth::delete_image_handler,
+                auth::update_profile_handler,
+                auth::admin_redirect,
+            ],
+        )
         .register("/", catchers![auth::unauthorized])
         .mount("/static", FileServer::from(image_dir))
         .attach(Template::fairing())
@@ -180,11 +198,13 @@ fn rocket() -> _ {
 fn backfill_image_dimensions() {
     let pool = db::init_pool();
     let mut conn = pool.get().expect("Failed to get DB connection");
-    
-    use diesel::prelude::*;
+
     use crate::schema::images::dsl::*;
-    let all_images = images.load::<crate::models::Image>(&mut conn).expect("Failed to load images");
-    
+    use diesel::prelude::*;
+    let all_images = images
+        .load::<crate::models::Image>(&mut conn)
+        .expect("Failed to load images");
+
     for img in all_images {
         let image_dir = std::env::var("IMAGE_DIR").unwrap_or_else(|_| "static".to_string());
         use std::path::Path;
@@ -194,17 +214,14 @@ fn backfill_image_dimensions() {
             } else {
                 Path::new(&image_dir).join(filename)
             };
-            
+
             if file_path.exists() {
                 if let Ok(dim) = imagesize::size(&file_path) {
                     let w = dim.width as i32;
                     let h = dim.height as i32;
                     println!("Backfilling image {}: {}x{}", img.path, w, h);
                     diesel::update(images.filter(id.eq(img.id)))
-                        .set((
-                            width.eq(Some(w)),
-                            height.eq(Some(h)),
-                        ))
+                        .set((width.eq(Some(w)), height.eq(Some(h))))
                         .execute(&mut conn)
                         .expect("Failed to update image");
                 } else {
@@ -216,4 +233,3 @@ fn backfill_image_dimensions() {
         }
     }
 }
-
